4a3cc2cbe70062b3d08b4e63505af19d9c930e6d
Vitaliy Mysak <vitaliy@ethworks.io>
2021-07-25 23:18:07 +0200

    TEST ALL THE OTHERS

--- packages/page-accounts/src/Accounts/index.spec.ts
+++ packages/page-accounts/src/Accounts/index.spec.ts
@@ -1,15 +1,17 @@
 // Copyright 2017-2021 @polkadot/page-accounts authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import type BN from 'bn.js';
+
 import { within, screen } from '@testing-library/react';
 
 import i18next from '@polkadot/react-components/i18n';
 import { MemoryStore } from '@polkadot/test-support/keyring';
 import { keyring } from '@polkadot/ui-keyring';
-import { formatBalance } from '@polkadot/util';
+import { BN_ZERO, formatBalance } from '@polkadot/util';
 
 import { AccountsPage } from '../../test/pages/accountsPage';
-import { someBalances } from '../../test/hooks/default';
+import { someBalances, someStakingAccount } from '../../test/hooks/default';
 
 describe('Accounts page', () => {
   let accountsPage: AccountsPage;
@@ -63,6 +65,11 @@ describe('Accounts page', () => {
       expect(accountRows).toHaveLength(2);
     });
 
+    function showBalance(amount: string | number | BN) {
+      // Formats exactly as <FormatBalance /> component does it
+      return formatBalance(amount, { decimals: 12, forceUnit: '-', withUnit: false });
+    }
+
     it('an account row displays the total balance info', async () => {
       accountsPage.renderPage([
         { id: '5DAAnrj7VHTznn2AWBemMuyBwZWs6FNFjdyVXUeYum3PTXFy', totalBalance: 10000 },
@@ -73,15 +80,15 @@ describe('Accounts page', () => {
       const balance = await within(rows[0]).findByTestId('balance-summary');
 
       const expectedAmount = '1050000000000'; // = Free + Reserved from mocked APIs
-      const expectedText = formatBalance(expectedAmount, { decimals: 12, withUnit: false });
+      const expectedText = showBalance(expectedAmount);
 
       expect(balance).toHaveTextContent(expectedText);
 
       // check that we don't compare 0.0000 vs 0.0000 because of rounding
-      expect(expectedText).not.toBe(formatBalance('0', { decimals: 12, withUnit: false }));
+      expect(expectedText).not.toBe(showBalance(0));
     });
-    
-    it('total balance summary displays the total balance', async () => {
+ 
+    it('summary displays balance', async () => {
       accountsPage.renderPage([
         { id: '5DAAnrj7VHTznn2AWBemMuyBwZWs6FNFjdyVXUeYum3PTXFy', totalBalance: 10000 },
         { id: '5HGjWAeFDfFCWPsjFQdVV2Msvz2XtMktvgocEZcCj68kUMaw', totalBalance: 999 }
@@ -89,10 +96,80 @@ describe('Accounts page', () => {
 
       // Two accounts have Free + Reserved balance
       const expectedAmount = someBalances.freeBalance.add(someBalances.reservedBalance).muln(2);
-      const expectedText = formatBalance(expectedAmount, { decimals: 12, withUnit: false });
+      const expectedText = showBalance(expectedAmount);
 
-      const summary = await screen.findByTestId(/card-summary:total balance/i);
+      const summary = await screen.findByTestId(/card-summary:(total )?balance/i);
       expect(summary).toHaveTextContent(expectedText);
     });
+ 
+    it('summary displays transferrable', async () => {
+      accountsPage.renderPage([
+        { id: '5DAAnrj7VHTznn2AWBemMuyBwZWs6FNFjdyVXUeYum3PTXFy', totalBalance: 10000 },
+        { id: '5HGjWAeFDfFCWPsjFQdVV2Msvz2XtMktvgocEZcCj68kUMaw', totalBalance: 999 }
+      ]);
+
+      const expectedAmount = someBalances.availableBalance.muln(2);
+      const expectedText = showBalance(expectedAmount);
+
+      const summary = await screen.findByTestId(/card-summary:(total )?transferrable/i);
+      expect(summary).toHaveTextContent(expectedText);
+    });
+ 
+    it('summary displays locked', async () => {
+      accountsPage.renderPage([
+        { id: '5DAAnrj7VHTznn2AWBemMuyBwZWs6FNFjdyVXUeYum3PTXFy', totalBalance: 10000 },
+        { id: '5HGjWAeFDfFCWPsjFQdVV2Msvz2XtMktvgocEZcCj68kUMaw', totalBalance: 999 }
+      ]);
+
+      const expectedAmount = someBalances.lockedBalance.muln(2);
+      const expectedText = showBalance(expectedAmount);
+
+      const summary = await screen.findByTestId(/card-summary:(total )?locked/i);
+      expect(summary).toHaveTextContent(expectedText);
+    });
+ 
+    it('summary displays bonded', async () => {
+      accountsPage.renderPage([
+        { id: '5DAAnrj7VHTznn2AWBemMuyBwZWs6FNFjdyVXUeYum3PTXFy', totalBalance: 10000 },
+        { id: '5HGjWAeFDfFCWPsjFQdVV2Msvz2XtMktvgocEZcCj68kUMaw', totalBalance: 999 }
+      ]);
+
+      const expectedAmount = someStakingAccount.stakingLedger.active.unwrap().muln(2);
+      const expectedText = showBalance(expectedAmount);
+
+      const summary = await screen.findByTestId(/card-summary:(total )?bonded/i);
+      expect(summary).toHaveTextContent(expectedText);
+    });
+ 
+    it('summary displays unbonding', async () => {
+      accountsPage.renderPage([
+        { id: '5DAAnrj7VHTznn2AWBemMuyBwZWs6FNFjdyVXUeYum3PTXFy', totalBalance: 10000 },
+        { id: '5HGjWAeFDfFCWPsjFQdVV2Msvz2XtMktvgocEZcCj68kUMaw', totalBalance: 999 }
+      ]);
+
+      // Unbonding amount is the sum of "unlocking" for each account
+      const expectedAmount = someStakingAccount
+        .unlocking?.reduce((acc, { value }) => acc.add(value), BN_ZERO)
+        .muln(2) ?? BN_ZERO;
+      const expectedText = showBalance(expectedAmount);
+
+      const summary = await screen.findByTestId(/card-summary:(total )?unbonding/i);
+      expect(summary).toHaveTextContent(expectedText);
+    });
+ 
+    it('summary displays redeemable', async () => {
+      accountsPage.renderPage([
+        { id: '5DAAnrj7VHTznn2AWBemMuyBwZWs6FNFjdyVXUeYum3PTXFy', totalBalance: 10000 },
+        { id: '5HGjWAeFDfFCWPsjFQdVV2Msvz2XtMktvgocEZcCj68kUMaw', totalBalance: 999 }
+      ]);
+
+      // Unbonding amount is the sum of "unlocking" for each account
+      const expectedAmount = someStakingAccount.redeemable?.muln(2) ?? BN_ZERO;
+      const expectedText = showBalance(expectedAmount);
+
+      const summary = await screen.findByTestId(/card-summary:(total )?redeemable/i);
+      expect(summary).toHaveTextContent(expectedText);
+    });
+
   });
 });
